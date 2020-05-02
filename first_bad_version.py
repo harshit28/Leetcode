c201aa63df64408850cddfb7a009b3134a5be574
harshit28 <hambalkar@gmail.com>
2020-05-01 17:54:58 -0700

    May1Leetcode

--- first_bad_version.py
+++ first_bad_version.py
@@ -6,7 +6,6 @@ class Solution:
         :rtype: int
         """
         l,h=0,n
-        # arr=[i for i in range(1,n+1)]
         while l<h:
             mid = l+(h-l)//2
             if not isBadVersion(mid):
