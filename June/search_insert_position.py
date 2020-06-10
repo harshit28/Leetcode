f64b6cc2dd7b0ffd5b095fc83d8b9b2cdac685f5
Harshit Ambalkar <hambalkar@gmail.com>
2020-06-10 00:23:16 -0700

    Create search_insert_position.py
    Leetcode day 10

---
+++ June/search_insert_position.py
@@ -0,0 +1,10 @@
+low,high=0,len(nums)-1
+        while(low<=high):
+            mid=low+(high-low)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high=mid-1
+            else:
+                low=mid+1
+        return low
