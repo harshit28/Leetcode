04fe5daccd45f385f66e53ee6fe2b017cf97b779
Harshit Ambalkar <hambalkar@gmail.com>
2021-01-12 11:03:23 -0800

    Create 876.py
    using faster and slow pointers it can be solved.

---
+++ 876.py
@@ -0,0 +1,9 @@
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        fast= slow = head 
+        
+        while fast and fast.next:
+            slow= slow.next
+            fast=fast.next.next
+            
+        return slow
