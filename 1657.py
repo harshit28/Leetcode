21bebcf934880a881b87752fbf6bbfbda126adeb
Harshit Ambalkar <hambalkar@gmail.com>
2021-01-22 21:22:50 -0800

    Create 1657.py

---
+++ 1657.py
@@ -0,0 +1,16 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        w1 = Counter(word1)
+        w2 = Counter(word2)
+        
+        letters1 = [x for x in w1.keys()]
+        letters2 = [x for x in w2.keys()]
+        
+        
+        
+        freq1 = [x for x in w1.values()]
+        freq2 = [x for x in w2.values()]
+
+        
+        print(letters1,letters2,freq1,freq2)
+        return sorted(letters1) == sorted(letters2) and sorted(freq1) == sorted(freq2)
