2157f15285ce1da06f1f390c4f1d5f941ffa93b1
Harshit Ambalkar <hambalkar@gmail.com>
2021-01-31 19:06:12 -0800

    Create 31.py

---
+++ Janurary21/31.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        for i in range(n-1, 0, -1):
+            if nums[i] > nums[i-1]:
+                j = i
+                while j < n and nums[j] > nums[i-1]:
+                    idx = j
+                    j += 1
+                nums[idx], nums[i-1] = nums[i-1], nums[idx]
+                for k in range((n-i)//2):
+                    nums[i+k], nums[n-1-k] = nums[n-1-k], nums[i+k]
+                break
+        else:
+            nums.reverse()3
