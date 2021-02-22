5aee28cab2ce4a31aeb40cd49c8979b20dfc6bc1
Harshit Ambalkar <hambalkar@gmail.com>
2021-02-22 12:07:22 -0800

    Create 524.py

---
+++ Feb21/524.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findLongestWord(self, s: str, d: List[str]) -> str:
+   
+        d.sort(key = lambda x: (-len(x), x))
+
+        index = 0
+        maxc=""
+        
+        for word in d:
+            i= 0
+            for char in s:
+                if i < len(word) and char == word[i]:
+                    i+=1
+            if i == len(word):
+                return word
+                            
+        return ""
